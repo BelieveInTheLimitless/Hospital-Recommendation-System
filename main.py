ddef5d5dcadeb30fc8a3ac82991c0e0e320063a5
kshitij <kshitijsalunke139@gmail.com>
2023-01-27 18:18:44 +0530

    Added code to create CSV file and store data in it

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+import pandas as pd
 
-url = "https://www.swiggy.com"
-r = requests.get(url)
-content = r.content
-soup = BeautifulSoup(content, 'html.parser')
+source = requests.get("https://www.swiggy.com/city/pune/kothrud-restaurants").text
 
+soup = BeautifulSoup(source, 'lxml')
+
+
+data = {
+    'Name': [],
+    'Type': [],
+    'Rating': [],
+    'Delivery Time': [],
+    'Price': []
+}
+
+
+
+for restaurant in soup.find_all('div', class_='_3XX_A'):
+
+    name = restaurant.find('div', class_='nA6kb').text
+
+    type = restaurant.find('div', class_='_1gURR').text
+
+    details = restaurant.find('div', class_='_3Mn31').text
+    details = details.split('•')
+
+    rating = details[0]
+
+    minutes = details[1]
+
+    price_for_two = details[2]
+
+
+    data['Name'].append(name)
+    data['Type'].append(type)
+    data['Rating'].append(rating)
+    data['Delivery Time'].append(minutes)
+    data['Price'].append(price_for_two[1:])
+
+
+df = pd.DataFrame(data)
+
+df.to_csv('restaurants_data.csv')
