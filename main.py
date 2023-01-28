34498af9c4145a9350608b9692bed859f1a6818c
Adnansk01 <iitcourse@hotmail.com>
2023-01-28 20:47:51 +0530

    More areas in pune

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 
 pages = np.arange(1, 8, 1)
 
+areas = ["thergaon", "varye-bk", "lonikand", "kolwadi", "alandi-chorachi", "pimpalgaon-mahalunge", "kuruli", "kalewadi", "shivajinagar", "bibvewadi", "hadpsar-ie", "khadki-bazar", "mohamadwadi", "chinchwadgaon", "phursungi", "karvenagar", "n-i-b-m", "viman-nagar", "baner-road", "kothrud", "navsahyadri", "sadashiv-peth", "khadakwal", "ghotondi", "sonari", "varale", "pimpri-bk", "vasuli", "pimpri", "kolvadi", "mulanagar", "sate", "saygaon", "kondhwa-kh", "wakad", "malewadi", "hadapsar", "khadki", "parvati", "shivajinagar-pune", "deccan-gymkhana", "swargate-chowk", "dapodi", "bhukum", "kondhwa-lh", "kalas", "tathawade", "khadaki", "baner", "airport-pune", "market-yard-pune", "akurdi", "dehu", "lonavala-bazar", "shivnagar", "vagholi", "katraj", "aundh-pune"]
 
 data = {
     'Name': [],
@@ -15,33 +16,36 @@ data = {
 }
 
 for page in pages:
-    source = requests.get("https://www.swiggy.com/city/pune/kothrud-restaurants?page=" + str(page)).text
 
-    soup = BeautifulSoup(source, 'lxml')
+    for area in areas:
+        
+        source = requests.get("https://www.swiggy.com/city/pune/" + str(area) + "-restaurants?page=" + str(page)).text
 
-    for restaurant in soup.find_all('div', class_='_3XX_A'):
+        soup = BeautifulSoup(source, 'lxml')
 
-        name = restaurant.find('div', class_='nA6kb').text
+        for restaurant in soup.find_all('div', class_='_3XX_A'):
 
-        type = restaurant.find('div', class_='_1gURR').text
+            name = restaurant.find('div', class_='nA6kb').text
 
-        details = restaurant.find('div', class_='_3Mn31').text
-        details = details.split('•')
+            type = restaurant.find('div', class_='_1gURR').text
 
-        rating = details[0]
+            details = restaurant.find('div', class_='_3Mn31').text
+            details = details.split('•')
 
-        minutes = details[1]
+            rating = details[0]
 
-        price_for_two = details[2]
+            minutes = details[1]
 
-
-        data['Name'].append(name)
-        data['Type'].append(type)
-        data['Rating'].append(rating)
-        data['Delivery Time'].append(minutes)
-        data['Price'].append(price_for_two[1:])
+            price_for_two = details[2]
 
 
-    df = pd.DataFrame(data)
+            data['Name'].append(name)
+            data['Type'].append(type)
+            data['Rating'].append(rating)
+            data['Delivery Time'].append(minutes)
+            data['Price'].append(price_for_two[1:])
+
+
+        df = pd.DataFrame(data)
 
 df.to_csv('restaurants_data.csv')
