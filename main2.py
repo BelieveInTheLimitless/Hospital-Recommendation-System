bdcfe564ae4a02074e271a269fa222065b1b3ae2
kshitij <kshitijsalunke139@gmail.com>
2023-03-04 16:14:49 +0530

    Changed main2.py and added hospitals_data.csv

--- main2.py
+++ main2.py
@@ -1,24 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import csv
 import pandas as pd
-import requests
-import numpy as np
 import time
 
 
-iterate = []
-
-lat = 18.4481802
+lat = 18.4581802  # 18.4481802
 lon = 73.7737253
-# https://www.google.com/maps/search/hospital/@18.4481802,73.7737253,15z/data=!3m1!4b1
-for j in range(30):
-        for i in range(21):
+restart = 73.7737253  # 73.7737253
+
+for j in range(20):  #30
+        lon = restart
+        for i in range(15): #21
                 driver = webdriver.Firefox(executable_path=r'E:\Firefox\geckodriver.exe')
                 url = "https://www.google.com/maps/search/hospital/@"+str(lat)+","+str(lon)+",16z"
                 driver.get(url)
 
-                time.sleep(7)
+                time.sleep(3)
 
 
                 data = {
@@ -30,16 +27,14 @@ for j in range(30):
 
 
                 hospitals_list = driver.find_elements(By.CSS_SELECTOR, 'a.hfpxzc')
-                ratings = driver.find_elements(By.CLASS_NAME, "AJB7ye")
+                ratings = driver.find_elements_by_class_name("AJB7ye")
 
-                hosp_info = driver.find_elements(By.CLASS_NAME, "W4Efsd")
+                hosp_info = driver.find_elements_by_class_name("W4Efsd")
                 type_hosp = []
 
-                #print(len(hospitals_list))
                 for k in range(len(hosp_info)):
                         s = hosp_info[k].text
                         if s.find(")") > 0 or s.find("No reviews") == 0:
-                                # print(hosp_info[i + 1].text, end="\n\n")
                                 type_hosp.append(hosp_info[k + 1].text)
 
                 for k in range(len(hospitals_list)):
@@ -75,7 +70,7 @@ for j in range(30):
                 # for hospital in hospitals_list:
                 #     print(hospital.get_attribute('aria-label'))
                 #     iterate.append(hospital.get_attribute('aria-label'))
-                lon = lon + 0.015
+                lon = lon + 0.015  # 0.015
                 driver.quit()
 
                 df = pd.DataFrame(data)
@@ -84,4 +79,7 @@ for j in range(30):
                 else:
                         df.to_csv('hospitals_data.csv', mode='a', index=False, header=False)
 
-        lat = lat + 0.010
+        lat = lat + 0.010  # 0.012
+
+
+
