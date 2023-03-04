7b6bddfb724f16453c97e83045cb065325f686ba
Vishal Patil <believeinthelimitless@gmail.com>
2023-03-04 16:26:09 +0530

    main.py improvements

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ from selenium.webdriver.common.by import By
 
 # Set up the driver
 driver = webdriver.Firefox()
+suburbs = []
+subs = open('areas.txt', 'r')
+for line in subs:
+    line = line.lower()
+    suburbs.append(line)
+subs.close
+print(suburbs)
 
 # Load the page
-url = "https://www.google.com/search?q=laptop+screen+height+in+pixels&ei=ABMCZPT7E6jJz7sPyKuoSA&oq=24.5+c&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAxgCMgQIABBDMgUIABCABDIFCAAQgAQyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHkoECEEYAFAAWABghRVoAHAAeACAAc0BiAHNAZIBAzItMZgBAMABAQ&sclient=gws-wiz-serp"
+url = "https://www.google.com/maps/search/hospital+in+"+str(suburbs[0])+"/"
 driver.get(url)
 time.sleep(5)
 scroll_pause_time = 5
@@ -19,9 +26,9 @@ hospitals_list = []
 
 while True:
     # Find all hospitals on the current page
-    #hospitals = driver.find_elements(By.CSS_SELECTOR, 'a.hfpxzc')
+    hospitals = driver.find_elements(By.CSS_SELECTOR, 'a.hfpxzc')
     # Append the hospitals to the hospitals_list
-    #hospitals_list.extend(hospitals)
+    hospitals_list.extend(hospitals)
     
     # scroll one screen height each time
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")  
