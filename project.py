905efd3e355427834bc061b6f50ce687b57fbb03
Adnansk01 <iitcourse@hotmail.com>
2023-04-13 16:41:16 +0530

    Address added

--- project.py
+++ project.py
@@ -15,13 +15,23 @@ data = pd.read_csv('output_file.csv')
 def extract_type(Info):
     # Split the address string by comma
     parts = Info.split('·')
+    # print(parts[1])
     parts = parts[0].split(',')
     # The city name should be the second-to-last part
     hos_type = parts[0].strip()
     return hos_type
 
+# Define a function to extract the hospital type
+def extract_address(Info):
+    # Split the address string by comma
+    parts = Info.split('·')
+    print(parts)
+    address = parts[1]
+    return address
+
 # Apply the function to the 'Address' column to extract the city name
 data['Type_hospital'] = data['Info'].apply(extract_type)
+data['Address_Contact'] = data['Info'].apply(extract_address)
 
 # Save the updated DataFrame to a new CSV file
 data.to_csv('modified.csv', index=False)
