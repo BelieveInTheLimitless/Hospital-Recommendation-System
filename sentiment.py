78d5a86fbd8200d3b57eeb6f9619eb80166e10c1
Adnansk01 <iitcourse@hotmail.com>
2023-04-19 22:04:44 +0530

    Some changes

--- sentiment.py
+++
@@ -1,8 +0,0 @@
-import re
-sadwords = []
-with open('emotions.txt','r') as file :
-    for line in file :
-        m = re.search("'(.*?)': 'sad'", line)
-        if m:
-            sadwords.append(m.group(1))
-print(sadwords)
